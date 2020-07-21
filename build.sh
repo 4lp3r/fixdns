fb7075510084c389a07465d1977ba3c9c85be5ef
4lp3r <55599607+4lp3r@users.noreply.github.com>
2020-07-21 17:26:13 +0300

    This script install other depencies...

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install figlet
+sudo apt-get install lolcat
+sudo pip3 install colorama
+sudo figlet "Completed !" | lolcat -S 1 -p 1
